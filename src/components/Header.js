ac9f8cdfdbb5546aca48331352c669e9b2ea4cc1
sonic11829 <sethneal15@ymail.com>
2023-04-24 21:24:50 -0400

    coded some of HomePage.js and Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import SearchBar from "./SearchBar";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Employee List</h1>
+      <SearchBar />
+    </>
+  )
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import PageBody from "./PageBody";
+import Header from "./Header"
+
+export default function HomePage(props) {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
